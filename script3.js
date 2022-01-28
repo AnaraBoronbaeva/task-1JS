6f708f779aeceacdcf2889363dd8bec214b3157b
Anara <87765773+AnaraBoronbaeva@users.noreply.github.com>
2022-01-28 16:59:31 +0600

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+// 3.Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let userName = "Anara";
+console.log(`Username:`+ userName);
+
+let userEmail = "anara.boronbaeva.kk@gmail.com";
+console.log(`User-email:`+ userEmail);
